22574ac8ef8a49fb5c11e05b6e2f7f7f5ddd1d92
kyan <kyanssharma10@gmail.com>
2022-07-12 05:04:34 +0530

    graph protocol added

--- web3-blog/config.js
+++ web3-blog/config.js
@@ -1,4 +1,4 @@
 
-  export const contractAddress = "0xa93afd35e02fF5aD10EF5B346c740945D3FEd9Aa"
+  export const contractAddress = "0x7602cB7c072BCa2E086a646dE19076a51F3dFb8c"
   export const ownerAddress = "0xf045aa3e2C9088F7988A736Ed3651b492fc21Dd5"
   
